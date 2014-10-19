e84703f1e160d8f413c266747f91af9be67cb8b0
Ron Kurr <kurr@kurron.org>
2014-10-19 12:09:51 -0400

    Finishing touches on using mounted volumes

--- container-linking/launch-containers.sh
+++ container-linking/launch-containers.sh
@@ -3,9 +3,8 @@
 # launch MongoDB, don't map any ports and use /tmp/mongodb-data for persistent storage
 docker run --detach --hostname="mongodb" --name="mongodb" --memory="512m" -v /tmp/mongodb-data:/data mongo:latest 
 
-# launch RabbitMQ, don't map any ports and use /var/lib/rabbitmq/mnesia for persistent storage running as the rabbitmq user
-#docker run --detach --hostname="rabbitmq" --name="rabbitmq" --memory="256m" --user="rabbitmq" -v /tmp/rabbitmq/log:/data/log -v /tmp/rabbitmq/mnesia:/data/mnesia kurron/rabbitmq:latest 
-docker run --detach --hostname="rabbitmq" --name="rabbitmq" --memory="256m" -v /tmp/rabbitmq/log:/data/log -v /tmp/rabbitmq/mnesia:/data/mnesia dockerfile/rabbitmq:latest 
+# launch RabbitMQ, don't map any ports and use /tmp/rabbitmq for persistent storage
+docker run --detach --hostname="rabbitmq" --name="rabbitmq" --memory="256m" -v /tmp/rabbitmq/log:/data/log -v /tmp/rabbitmq/mnesia:/data/mnesia kurron/rabbitmq:3.3.5 
 
 # launch the application, map its port to localhost:8080 and have Docker take care of mapping the MongoDB and RabbitMQ ports to this container
 # Noticed that we use an alias for mongodb. That is because by default the application is expecting a hostname of mongo for the database
