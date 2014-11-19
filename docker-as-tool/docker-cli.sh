ca7ef7ec122cd4917698766aef54c7d02fe50fc1
Ron Kurr <rkurr@transparent.com>
2014-11-19 16:34:21 -0500

    Fixed the user permission problem

--- docker-as-tool/docker-cli.sh
+++ docker-as-tool/docker-cli.sh
@@ -3,4 +3,4 @@
 # run this script giving it a single argument eg. ./docker-cli.sh ronbo-was-here
 # that will attempt to create an empty file name ronbo-was-here in the current directory
 
-docker run --volume $(pwd):/src --workdir=/src --user=vagrant --rm --attach stdout --attach stderr --interactive toolbox "$@" 
+docker run --volume $(pwd):/src --workdir=/src --user=$(id -u $(whoami)):$(id -g $(whoami)) --env RUNAS=$USER  --rm --attach stdout --attach stderr --interactive toolbox "$@" 
