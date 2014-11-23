5b4c7ada7838f61a62cda3432f9465f8f2a656d5
Ron Kurr <kurr@kurron.org>
2014-11-23 01:19:42 +0000

    Made image smaller

--- gradle-toolbox/build-image.sh
+++ gradle-toolbox/build-image.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 docker build --tag="kurron/gradle:2.2.0" .
+docker images
--- gradle-toolbox/inspect-container.sh
+++ gradle-toolbox/inspect-container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm --tty --interactive --hostname="gradle" --name="gradle" --memory="256m" kurron/gradle:2.2.0 /bin/bash
+docker run --rm --tty --interactive --hostname="gradle" --name="gradle" --memory="256m" --entrypoint="/bin/bash" kurron/gradle:2.2.0
