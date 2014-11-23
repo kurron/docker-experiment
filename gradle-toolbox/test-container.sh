75d00d7c58cf3d267f6dd2ed4123a31cbca25488
Ron Kurr <kurr@kurron.org>
2014-11-23 02:12:57 +0000

    Improved the library build

---
+++ gradle-toolbox/test-container.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run --volume $(pwd):/src --workdir=/src --user=$(id -u $(whoami)):$(id -g $(whoami)) --rm --attach stdout --attach stderr --interactive kurron/gradle:2.2.0 "$@"
