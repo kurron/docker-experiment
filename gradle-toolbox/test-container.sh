0932dd4d965d79e390a212be0b9af1a5d37a33ef
Ron Kurr <kurr@kurron.org>
2014-11-24 01:02:20 +0000

    Account for stupid Gradle and Maven settings that create the ? folder.

--- gradle-toolbox/test-container.sh
+++ gradle-toolbox/test-container.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker run --volume $(pwd):/src --workdir=/src --user=$(id -u $(whoami)):$(id -g $(whoami)) --rm --attach stdout --attach stderr --interactive kurron/gradle:2.2.0 "$@"
+docker run --volume $(pwd):/src --workdir=/src --user=$(id -u $(whoami)):$(id -g $(whoami)) --rm --attach stdout --attach stderr --interactive kurron/gradle:2.2.0 "$@" 
+
